71a0f04165717a004b7bf47f86f08baa0dde7558
Jacob N <levolpehh@hotmail.com>
2023-03-30 14:51:51 -0400

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -102,13 +102,13 @@ $auth = isset($_SESSION["user"]);
     <?= $yield ?? null ?>
 
     <footer class="my-5 pt-5 text-muted text-center text-small">
-    <p class="mb-1">&copy; 2017-2023 Helpets</p>
-    <ul class="list-inline">
-      <li class="list-inline-item"><a href="#">Privacy</a></li>
-      <li class="list-inline-item"><a href="#">Terms</a></li>
-      <li class="list-inline-item"><a href="#">Support</a></li>
-    </ul>
-  </footer>
+        <p class="mb-1">&copy; 2017-2023 Helpets</p>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="#">Privacy</a></li>
+            <li class="list-inline-item"><a href="#">Terms</a></li>
+            <li class="list-inline-item"><a href="#">Support</a></li>
+        </ul>
+    </footer>
 </body>
 
 </html>
