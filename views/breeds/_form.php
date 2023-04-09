c92b1562a1a3d93db6abfef1dc4a43b23da17618
Nathan Marino <Marinonathan11101@gmail.com>
2023-04-09 17:13:59 -0400

    fixed the breed page

--- views/breeds/_form.php
+++ views/breeds/_form.php
@@ -6,7 +6,7 @@
 
 <form action="<?= ROOT_PATH ?>/breeds/<?= $action ?>" method="post">
     <?php if ($action === "update"): ?>
-        <input type="hidden" name="animal_id" value="<?= $form_fields["animal_id"] ?>">
+        <input type="hidden" name="breed_id" value="<?= $form_fields["breed_id"] ?>">
     <?php endif ?>
 
     <div class="form-group my-3">
